cc67af6f9f98d174bd8d40aa4302f484559071b5
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-01-21 16:06:54 +0000

    Added reactstrap, fonts

---
+++ src/components/MainNavbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Navbar } from 'reactstrap';
+
+class MainNavbar extends Component {
+  render() {
+    return (
+      <div className='MainNavbar'>
+        <Navbar>
+          <h1>Hola!!!</h1>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default MainNavbar;
