2b9dd995489de76bee13db30c0da51deeeef4782
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-01-30 02:00:09 +0000

    Added value to country selected

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,16 +6,18 @@ import CountryCard from './CountryCard';
 class Main extends Component {
   constructor(props) {
     super(props);
+
+    this.onCountryChange = this.onCountryChange.bind(this);
     this.state = {
-      selectedCountry: null,
+      value: 'Worldwide',
     };
   }
 
   onCountryChange(event) {
-    // this.setState = { selectedCountry: this.country.countryInfo.iso2 };
-    console.log(event);
+    this.setState({ value: event.target.value });
+    event.preventDefault();
+    console.log(event.target.value);
   }
-
   render() {
     const countryList = countries.map((country) => {
       return <option key={country.countryInfo.iso2}>{country.country}</option>;
@@ -25,19 +27,27 @@ class Main extends Component {
       <div class='Main'>
         <div className='container py-3'>
           <h3 className=' text-center'>Coronavirus Data by Country</h3>
-          <Form>
-            <FormGroup className='row py-3'>
-              <Input
-                type='select'
-                id='selectCountry'
-                name='selectCountry'
-                className='col-7 mx-auto'
-                onChange={this.onCountryChange}
-              >
-                {countryList}
-              </Input>
-            </FormGroup>
-          </Form>
+          <div className='row'>
+            <div className='col-md-6'>
+              <h4 className='text-center mt-2 ' key={this.state.value}>
+                {this.state.value}
+              </h4>
+            </div>
+            <Form className='col text-center'>
+              <FormGroup className='py-3'>
+                <select
+                  value={this.state.value}
+                  id='selectCountry'
+                  name='selectCountry'
+                  className='col-7 mx-auto'
+                  onChange={this.onCountryChange}
+                >
+                  {countryList}
+                </select>
+              </FormGroup>
+            </Form>
+          </div>
+
           <CountryCard />
         </div>
       </div>
