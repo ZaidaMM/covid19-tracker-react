d868dfe8199e9aaa3c73c6d914db77122718fe83
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-02-25 02:40:20 +0000

    Added Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,52 +3,54 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div className='container-fluid main-color'>
-        <div class='row py-4'>
-          <div className='col-4 offset-1'>
-            <ul className='list-unstyled mt-2 '>
-              <li>
-                <a href='#' className='text-color'>
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href='#' className='text-color'>
-                  Data
-                </a>
-              </li>
-              <li>
-                <a href='#' className='text-color'>
-                  About
-                </a>
-              </li>
-              <li>
-                <a href='#' className='text-color'>
-                  Contact
-                </a>
-              </li>
-            </ul>
+      <footer className='footer'>
+        <div className='container-fluid main-color text-small'>
+          <div className='row'>
+            <div className='col-4 offset-2'>
+              <ul className='list-unstyled mt-2 '>
+                <li>
+                  <a href='#' className='text-color'>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href='#' className='text-color'>
+                    Stats
+                  </a>
+                </li>
+                <li>
+                  <a href='#' className='text-color'>
+                    Countries
+                  </a>
+                </li>
+                <li>
+                  <a href='#' className='text-color'>
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className='col-6 text-center align-self-center'>
+              <span className='text-color mx-2'>Follow us</span>{' '}
+              <a href='http://twitter.com/'>
+                <i className='fa fa-twitter text-color mx-1 icon'></i>
+              </a>
+              <a
+                href='http://linkedin.com/'
+                target='_blank'
+                rel='noreferrer noopener'
+              >
+                <i className='fa fa-linkedin text-color mx-1 icon'></i>
+              </a>
+            </div>
           </div>
-          <div className='col-6 text-center'>
-            <a href='http://instagram.com/'>
-              <i className='fas fa-instagram'></i>
-            </a>
-            <a href='http://twitter.com/'>
-              <i className='fa fa-twitter'></i>
-            </a>
-            <a href='http://instagram.com/'>
-              <i className='fa fa-instagram'></i>
-            </a>
+          <div className='row'>
+            <div className='col'>
+              <p className='text-color text-center'>Copyright &copy; 2021</p>
+            </div>
           </div>
         </div>
-        <div className='row'>
-          <div className='col'>
-            <p className='text-color text-center text-small'>
-              Copyright &copy; 2021
-            </p>
-          </div>
-        </div>
-      </div>
+      </footer>
     );
   }
 }
